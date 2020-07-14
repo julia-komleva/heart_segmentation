e9494ba337f9170c8706eb1e38023fa2d3d2222e
euloo <komleva.j@gmail.com>
2020-07-14 14:17:25 +0500

    sh acdc emidec

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,5 +1,6 @@
 import nibabel as nib
-import cv2
+# import cv2
+import matplotlib.pyplot as plt
 
 
 def nib_to_image():
@@ -7,14 +8,16 @@ def nib_to_image():
         img = nib.load(path_in)
         img = img.get_fdata()
         img = img[:, :, 0]
-        cv2.imwrite(path_out, img)
+        # cv2.imwrite(path_out, img) # problem
+        plt.imsave(path_out, img, format='jpeg', cmap='gray')
 # TODO: make binary or 3 classes
 def nib_to_mask():
     for path_in, path_out in zip(snakemake.input.masks, snakemake.output.masks):
         img = nib.load(path_in)
         img = img.get_fdata()
         img = img[:, :, 0]
-        cv2.imwrite(path_out, img)
+        # cv2.imwrite(path_out, img) # problem
+        plt.imsave(path_out, img, format='jpeg', cmap='gray')
 def nib_to_image_and_mask():
     nib_to_image()
     nib_to_mask()
---
+++ snakemake.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set execute permission on your script:
+# chmod +x snakemake.sh
+# To run your script, enter:
+# ./snakemake.sh
+snakemake --cores 1 -s snakefile_acdc
+snakemake --cores 1 -s snakefile_emidec
