2c26a280d0292342fe27661b17c092ad58647292
euloo <komleva.j@gmail.com>
2020-07-16 11:53:36 +0500

    loop

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,25 +1,36 @@
 import nibabel as nib
-# import cv2
-import matplotlib.pyplot as plt
+import cv2
+
 
 
 def nib_to_image():
     for path_in, path_out in zip(snakemake.input.images, snakemake.output.images):
         img = nib.load(path_in)
-        img = img.get_fdata()
-        img = img[:, :, 0]
-        # cv2.imwrite(path_out, img) # problem
-        plt.imsave(path_out, img, format='jpeg', cmap='gray')
+        img = img.get_fdata().astype(int)
+        n = img.shape[2]
+        for i in range(n):
+            slice = img[:, :, i]
+            path = path_out + str(i) + '.png'
+            cv2.imwrite(path, slice)  # problem
+        # plt.imsave(path_out, img, format='jpeg', cmap='gray')
+
+
 # TODO: make binary or 3 classes
 def nib_to_mask():
     for path_in, path_out in zip(snakemake.input.masks, snakemake.output.masks):
         img = nib.load(path_in)
-        img = img.get_fdata()
-        img = img[:, :, 0]
-        # cv2.imwrite(path_out, img) # problem
-        plt.imsave(path_out, img, format='jpeg', cmap='gray')
+        img = img.get_fdata().astype(int)
+        n = img.shape[2]
+        for i in range(n):
+            slice = img[:, :, i]
+            path = path_out + str(i) + '.png'
+            cv2.imwrite(path, slice)  # problem
+        # plt.imsave(path_out, img, format='jpeg', cmap='gray')
+
+
 def nib_to_image_and_mask():
     nib_to_image()
     nib_to_mask()
+
 
 nib_to_image_and_mask()
