952b032faf9917486a1b2ea949d48dcd92f19b85
euloo <komleva.j@gmail.com>
2020-07-16 12:11:19 +0500

    depth

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -2,12 +2,14 @@ import nibabel as nib
 import cv2
 
 
-
 def nib_to_image():
     for path_in, path_out in zip(snakemake.input.images, snakemake.output.images):
         img = nib.load(path_in)
         img = img.get_fdata().astype(int)
         n = img.shape[2]
+        m = img.max()
+        if m > 255:
+            img //= 16  # emidec depth
         for i in range(n):
             slice = img[:, :, i]
             path = path_out + str(i) + '.png'
