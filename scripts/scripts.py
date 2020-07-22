e1512de75420d2aa38162a9d3ec10b5bf575ec46
euloo <13154751+euloo@users.noreply.github.com>
2020-07-22 12:43:36 +0500

    unfinished scripts.py

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,38 +1,64 @@
 import nibabel as nib
 import cv2
-
+import nibabel as nib
+import cv2
+import re
+import numpy as np
 
 def nib_to_image():
     for path_in, path_out in zip(snakemake.input.images, snakemake.output.images):
         img = nib.load(path_in)
         img = img.get_fdata().astype(int)
         n = img.shape[2]
-        #img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX) # worse
+        img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX) # worse
+        i = int(re.sub('_', '', path_out[-6:-4]))
+        slice = img[:, :, i]
+        cv2.imwrite(path_out, slice)
 
-        for i in range(n):
-            slice = img[:, :, i]
-            slice = cv2.normalize(slice, None, 0, 255, cv2.NORM_MINMAX)  # worse
-            path = path_out + str(i) + '.png'
-            cv2.imwrite(path, slice)  # problem
-        # plt.imsave(path_out, img, format='jpeg', cmap='gray')
 
 
 # TODO: make binary or 3 classes
-def nib_to_mask():
+def nib_to_mask(preprocess):
+
     for path_in, path_out in zip(snakemake.input.masks, snakemake.output.masks):
         img = nib.load(path_in)
         img = img.get_fdata().astype(int)
+
+        if preprocess:
+            img = change_masks(img)
+
         n = img.shape[2]
-        for i in range(n):
-            slice = img[:, :, i]
-            path = path_out + str(i) + '.png'
-            cv2.imwrite(path, slice)  # problem
-        # plt.imsave(path_out, img, format='jpeg', cmap='gray')
+        i = int(re.sub('_', '', path_out[-9:-7]))
+        slice = img[:, :, i]
+        cv2.imwrite(path_out, slice)
 
 
-def nib_to_image_and_mask():
+# rename masks to unite classes in different datasets
+def change_masks(img, preprocess):
+    if preprocess == 'nothing':
+        return img
+    elif preprocess == 'intersection': # unite classes
+        if snakemake.params.dataset == 'emidec':
+            img[img == 4] = 6 # no-reflow class
+            img[img == 3] = 5 # myocardium infraction
+            img[img == 2] = 4 # cavity
+            img[img == 1] = 2 # myocardium (acdc class 2)
+            return img
+    elif preprocess == 'union':
+        if snakemake.params.dataset == 'emidec':
+            img[img == 4] = 8
+            img[img == 3] = 7
+            img[img == 2] = 6
+            img[img == 1] = 5
+            img[img == 0] = 4
+            return img
+def nib_to_image_and_mask(preprocess = 'union'):
     nib_to_image()
-    nib_to_mask()
+    nib_to_mask(preprocess)
 
 
 nib_to_image_and_mask()
+
+
+
+
